8ee2c8e18ff1431bc81c0ff5293f693b4cb36637
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-10 09:09:32 +0000

    feat(added-a-league-map-TypedDict-to-map-the-leagues-and-their-league-code): used a TypeDict to make sure the values are of the expected type. Also makes the dictioary more readable

---
+++ src/footcrawl/constants.py
@@ -0,0 +1,19 @@
+import typing as T
+
+
+LeagueMap = T.TypedDict('LeagueMap', {
+    "premier-league": str,
+    "la-liga": str,
+    "serie-a": str,
+    "bundesliga": str,
+    "ligue-1": str,
+})
+
+
+LEAGUE_MAP: LeagueMap = {
+    "premier-league": "GB1",
+    "la-liga": "ES1",
+    "serie-a": "IT1",
+    "bundesliga": "L1",
+    "ligue-1": "FR1",
+}
