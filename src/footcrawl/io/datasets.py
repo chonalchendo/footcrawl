25baaa60d01b10567e04a13cc37b94d4acf344d5
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-11 22:11:58 +0000

    feat(datasets.py): added dataset related io code to this file

---
+++ src/footcrawl/io/datasets.py
@@ -0,0 +1,36 @@
+import abc
+import typing as T
+
+import polars as pl
+import pydantic as pdt
+
+
+class Writer(abc.ABC, pdt.BaseModel, strict=True, frozen=True, extra="forbid"):
+    KIND: str
+    
+    @abc.abstractmethod
+    def write(self, data: pl.DataFrame) -> None:
+        pass
+
+
+class ParquetWriter(Writer):
+    KIND: T.Literal['ParquetWriter'] = "ParquetWriter"
+    
+    path: str
+    
+    @T.override
+    def write(self, data: pl.DataFrame) -> None:
+        data.write_parquet(self.path)
+        
+        
+class JsonWriter(Writer):
+    KIND: T.Literal['JsonWriter'] = 'JsonWriter'
+    
+    path: str
+    
+    @T.override
+    def write(self, data: pl.DataFrame) -> None:
+        data.write_json(self.path)
+
+
+WriterKind = ParquetWriter | JsonWriter
