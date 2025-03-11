1e6cdeee3423a49be56f2c74956f6c7f4cd3930f
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-11 22:02:38 +0000

    feat(cli.py): added code to take a yaml file as an input, parse it, and validate it with pydantic

---
+++ src/footcrawl/cli.py
@@ -0,0 +1,29 @@
+import argparse
+from rich import print
+
+
+from footcrawl.io import configs
+from footcrawl import settings
+
+parser = argparse.ArgumentParser(
+    description="Run a crawler job from YAML/JSON configuration files."
+)
+parser.add_argument("files", nargs="*", help="Config files for the job")
+
+
+def execute(argv: list[str] | None = None) -> int:
+    args = parser.parse_args(argv)
+    files = [configs.parse_file(file) for file in args.files]
+    
+    if len(files) == 0:
+        raise RuntimeError("No configs provided.")
+
+    config = configs.merge_configs(files)
+    config.pop('globals')  # remove global values
+    
+    object_ = configs.to_object(config)
+    setting = settings.MainSettings.model_validate(object_)
+    
+    print(setting)
+
+    return 0
