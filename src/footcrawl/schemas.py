ff2fb88b4f4c7bafd418b47ff207f448e5a95622
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-11 22:14:47 +0000

    refactor(schemas.py): changed type hints for three items

--- src/footcrawl/schemas.py
+++ src/footcrawl/schemas.py
@@ -4,6 +4,6 @@ import pydantic as pdt
 class Clubs(pdt.BaseModel):
     league: str = pdt.Field(...)
     season: int = pdt.Field(...)
-    tm_team_name: str = pdt.Field(...)
-    tm_team_id: int = pdt.Field(...)
-    team_name: str = pdt.Field(...)
+    tm_team_name: list[str] = pdt.Field(...)
+    tm_team_id: list[int] = pdt.Field(...)
+    team_name: list[str] = pdt.Field(...)
