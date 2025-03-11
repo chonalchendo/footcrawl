bd7680ab8951722ca232630b04efc28714cb5da1
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-11 22:13:27 +0000

    feat(sources.py): created classes to manage data sources that will be scraped for this project e.g. fbref, sofascore

---
+++ src/footcrawl/sources.py
@@ -0,0 +1,44 @@
+import abc
+import typing as T
+
+import pydantic as pdt
+
+from footcrawl import services, crawlers
+from footcrawl.io import datasets
+
+
+class Source(abc.ABC, pdt.BaseModel, strict=True, frozen=True, extra="forbid"):
+    KIND: str
+    
+    logger_service: services.LoggerService = services.LoggerService()
+
+    @abc.abstractmethod
+    def start(self) -> None:
+        pass
+
+
+class Transfermarkt(Source):
+    KIND: T.Literal["Transfermarkt"] = "Transfermarkt"
+
+    crawler: crawlers.CrawlerKind = pdt.Field(..., discriminator="KIND")
+    output: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
+
+    @T.override
+    def start(self) -> None:
+        logger = self.logger_service.logger()
+        logger.info("With logger {}", logger)
+        logger.info(
+            "Crawling {} for season {} in leagues {}",
+            self.crawler,
+            self.crawler.seasons,
+            self.crawler.leagues,
+        )
+
+        self.crawler.crawl()
+
+    @property
+    def to_dataframe(self):
+        return self.crawler.output_result
+
+
+SourceKind = Transfermarkt
