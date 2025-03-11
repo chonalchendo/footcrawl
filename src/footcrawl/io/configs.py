0a712777c1c8ee0d5d6736b73a0e90af15e365e2
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-11 22:06:38 +0000

    feat(configs.py): wrote code to parse data from a yaml file

---
+++ src/footcrawl/io/configs.py
@@ -0,0 +1,16 @@
+import typing as T
+import omegaconf as oc
+
+Config = oc.ListConfig | oc.DictConfig
+
+
+def parse_file(path: str) -> Config:
+    return oc.OmegaConf.load(path)
+
+
+def merge_configs(configs: T.Sequence[Config]) -> Config:
+    return oc.OmegaConf.merge(*configs)
+
+
+def to_object(config: Config, resolve: bool = True) -> object:
+    return oc.OmegaConf.to_container(config, resolve=resolve)
