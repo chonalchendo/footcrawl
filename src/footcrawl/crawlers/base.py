89eac2710f4d41c0d50386dac589c8863897d48b
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-16 13:53:06 +0000

    refactor(crawlers/base.py): added headers, url, metrics class to base crawler parameters

--- src/footcrawl/crawlers/base.py
+++ src/footcrawl/crawlers/base.py
@@ -3,13 +3,18 @@ import abc
 import pydantic as pdt
 
 from footcrawl.io import services
+from footcrawl import metrics
 
 
 class Crawler(abc.ABC, pdt.BaseModel, strict=True, frozen=False, extra="forbid"):
-    KIND: str
+    KIND: str\
+    
+    url: str
+    headers: dict[str, str]
 
     logger_service: services.LoggerService = services.LoggerService()
-
+    crawler_metrics: metrics.CrawlerMetrics = metrics.CrawlerMetrics()
+    
     @abc.abstractmethod
     async def crawl(self) -> None:
         pass
