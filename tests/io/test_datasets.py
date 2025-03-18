ecba6b8ad6a215b63199185495bad8f69cd34dd3
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-18 08:13:59 +0000

    test(test_datasets.py): added tests to test the writing of data to json file asynchronously

---
+++ tests/io/test_datasets.py
@@ -0,0 +1,24 @@
+# %% IMPORTS
+
+import os
+
+import pytest
+
+from footcrawl.io import datasets
+
+
+# %% WRITERS
+
+@pytest.mark.asyncio(loop_scope='session')
+async def test_async_json_writer(tmp_outputs_path: str) -> None:
+    # given
+    data = {
+        'hello': 'world',
+        'foo': 'bar'
+    }
+    
+    writer = datasets.AsyncJsonWriter(path=tmp_outputs_path)
+    # when
+    await writer.write(data=data)
+    # then
+    assert os.path.exists(tmp_outputs_path), "Data should be written!"
