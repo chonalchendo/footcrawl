04789c920786b95d333640360d9d21ceb7ef9a42
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-18 08:20:32 +0000

    test(test_base.py): added more asserts to test base crawler class

--- tests/crawlers/test_base.py
+++ tests/crawlers/test_base.py
@@ -37,3 +37,5 @@ async def test_crawler(
     # then
     assert hasattr(crawler, "logger_service"), "Crawler should have an Logger service!"
     assert hasattr(crawler, "crawler_metrics"), "Crawler should have a metrics class!"
+    assert hasattr(crawler, "url"), "Crawler should have a URL!"
+    assert hasattr(crawler, "headers"), "Crawler should have headers!" 
