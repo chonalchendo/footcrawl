633b99ce831db785b5e1035cda6234aedde192ce
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-18 08:12:56 +0000

    test(conftest.py): added configuration file for testing

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+# %% IMPORTS
+
+import os
+import typing as T
+
+import pytest
+import _pytest.logging as pl
+
+
+from footcrawl.io import services
+from footcrawl import metrics
+
+# %% FIXTURES
+
+# %% - Paths
+
+
+@pytest.fixture(scope="function")
+def tmp_outputs_path(tmp_path: str) -> str:
+    """Return a tmp path for the outputs dataset."""
+    return os.path.join(tmp_path, "outputs.json")
+
+
+# %% - Services
+
+
+@pytest.fixture(scope="session", autouse=True)
+def logger_service() -> T.Generator[services.LoggerService, None, None]:
+    """Return and start the logger service."""
+    service = services.LoggerService(colorize=False, diagnose=True)
+    service.start()
+    yield service
+    service.stop()
+    
+    
+@pytest.fixture
+def logger_caplog(
+    caplog: pl.LogCaptureFixture, logger_service: services.LoggerService
+) -> T.Generator[pl.LogCaptureFixture, None, None]:
+    """Extend pytest caplog fixture with the logger service (loguru)."""
+    # https://loguru.readthedocs.io/en/stable/resources/migration.html#replacing-caplog-fixture-from-pytest-library
+    logger = logger_service.logger()
+    handler_id = logger.add(
+        caplog.handler,
+        level=0,
+        format="{message}",
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
+    
+
+# %% - Metrics
+
+@pytest.fixture(scope='session')
+def crawler_metrics() -> metrics.CrawlerMetrics:
+    return metrics.CrawlerMetrics()
+    
