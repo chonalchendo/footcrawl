17fab0584a9526cff38c883556a314a192ac120d
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-20 19:04:34 +0000

    feat(cli.py): added in schema and extra cli parameters for flexibility and so that I can properly test code

--- src/footcrawl/cli.py
+++ src/footcrawl/cli.py
@@ -18,23 +18,29 @@ parser = argparse.ArgumentParser(
     description="Run a crawler job from YAML/JSON configuration files."
 )
 parser.add_argument("files", nargs="*", help="Config files for the job")
-parser.add_argument("-s", "--schema", action="store_true", help="Print settings schema and exit.")
+parser.add_argument(
+    "-s", "--schema", action="store_true", help="Print settings schema and exit."
+)
+parser.add_argument(
+    "-e", "--extras", nargs="*", default=[], help="Config strings for the job."
+)
 
 
 def execute(argv: list[str] | None = None) -> int:
     args = parser.parse_args(argv)
-    
+
     if args.schema:
         schema = settings.MainSettings.model_json_schema()
         json.dump(schema, sys.stdout, indent=4)
         return 0
-    
+
     files = [configs.parse_file(file) for file in args.files]
+    strings = [configs.parse_string(string) for string in args.extras]
 
     if len(files) == 0:
         raise RuntimeError("No configs provided.")
 
-    config = configs.merge_configs(files)
+    config = configs.merge_configs([*files, *strings])
 
     if not isinstance(config, oc.DictConfig):
         raise RuntimeError("Config is not a dictionary")
@@ -43,10 +49,10 @@ def execute(argv: list[str] | None = None) -> int:
 
     # parse user agent
     user_agent = os.getenv("USER_AGENT")
-    object_["crawler"]['http_client']["headers"]["User-Agent"] = user_agent
+    object_["crawler"]["http_client"]["headers"]["User-Agent"] = user_agent
 
     setting = settings.MainSettings.model_validate(object_)
-    
+
     # start the crawler
     asyncio.run(setting.crawler.crawl())
     return 0
