528fb38e9475b8d24fdd0cf42f8cb5bba7ae3417
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-20 19:08:04 +0000

    feat(configs.py): added parse string function to parse extra configs as a string and into a omegaconf format

--- src/footcrawl/io/configs.py
+++ src/footcrawl/io/configs.py
@@ -9,6 +9,10 @@ def parse_file(path: str) -> Config:
     return oc.OmegaConf.load(path)
 
 
+def parse_string(string: str) -> Config:
+    return oc.OmegaConf.create(string)
+
+
 def merge_configs(configs: T.Sequence[Config]) -> Config:
     return oc.OmegaConf.merge(*configs)
 
