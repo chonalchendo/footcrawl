16b1e9c3da70b9138b5d1a9039650feb10b171d2
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-26 19:57:41 +0000

    refactor(crawler/squads.py): refactored name of squads scraper

--- src/footcrawl/crawlers/squads.py
+++ src/footcrawl/crawlers/squads.py
@@ -3,10 +3,8 @@ import typing as T
 from footcrawl.crawlers import base
 
 
-class SquadsCrawler(base.Crawler):
-    KIND: T.Literal["Squads"] = "Squads"
-
-    url: str
+class AsyncSquadsCrawler(base.Crawler):
+    KIND: T.Literal["AsyncSquadsCrawler"] = "AsyncSquadsCrawler"
 
     @T.override
     async def crawl(self) -> base.Locals:
