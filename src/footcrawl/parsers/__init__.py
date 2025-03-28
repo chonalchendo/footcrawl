8f23839c3d2db9075dd486ae04101f46f68fd2b7
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-28 16:50:39 +0000

    feat(parsers): added a parser kind type

--- src/footcrawl/parsers/__init__.py
+++ src/footcrawl/parsers/__init__.py
@@ -2,4 +2,6 @@ from .base import Item
 from .clubs import ClubsParser
 from .squads import SquadsParser
 
+ParserKind = ClubsParser | SquadsParser
+
 __all__ = ["ClubsParser", "SquadsParser", "Item"]
