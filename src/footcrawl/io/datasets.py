32516b377d394893207f3b85c570a73f9cbc856e
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-28 13:13:34 +0000

    feat(datasets.py): added data loader base class that data loaders will inherit

--- src/footcrawl/io/datasets.py
+++ src/footcrawl/io/datasets.py
@@ -6,6 +6,55 @@ from pathlib import Path
 import pydantic as pdt
 
 StreamingOutput = dict[str, T.Any]
+LoadedInput = list[dict[str, T.Any]]
+
+# %% - LOADERS
+
+
+class Loader(abc.ABC, pdt.BaseModel, strict=True, frozen=False, extra="forbid"):
+    """Base class for all loaders.
+
+    Args:
+        path (str): Path to load the data.
+    """
+
+    KIND: str
+
+    path: str
+
+    @abc.abstractmethod
+    def load(self, season: int) -> list[dict[str, T.Any]]:
+        """Load the data from the path.
+
+        Returns:
+            list[dict[str, T.Any]]: The loaded data.
+        """
+        pass
+
+
+class JsonLoader(Loader):
+    KIND: T.Literal["JsonLoader"] = "JsonLoader"
+
+    @T.override
+    def load(self, season: int) -> list[dict[str, T.Any]]:
+        if season < 2010 and season > 2024:
+            raise ValueError("Season must be between 2010 and 2024.")
+
+        formatted_path = self.__format_path(season)
+
+        if not Path(formatted_path).exists():
+            raise FileNotFoundError(f"File not found: {formatted_path}")
+
+        with open(formatted_path, "r") as file:
+            return [json.loads(line) for line in file]
+
+    def __format_path(self, season: int) -> str:
+        return self.path.format(season=season)
+
+
+LoaderKind = JsonLoader
+
+# %% - WRITERS
 
 
 class Writer(abc.ABC, pdt.BaseModel, strict=True, frozen=False, extra="forbid"):
@@ -43,7 +92,7 @@ class AsyncJsonWriter(Writer):
 
     KIND: T.Literal["AsyncJsonWriter"] = "AsyncJsonWriter"
 
-    overwrite: bool = pdt.Field(default=True)
+    overwrite: bool = pdt.Field(default=True)  # checked if true in crawler files
 
     @T.override
     async def write(self, data: StreamingOutput) -> None:
