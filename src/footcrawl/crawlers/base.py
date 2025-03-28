da4386ce6b6f3a781bd01c5f8b69b9bdb6389418
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-28 16:49:41 +0000

    feat(crawlers/base.py): added an engine class that contains all the crawler logic

--- src/footcrawl/crawlers/base.py
+++ src/footcrawl/crawlers/base.py
@@ -1,10 +1,14 @@
 import abc
 import typing as T
+from pathlib import Path
 
+import aiohttp
 import pydantic as pdt
+from tenacity import retry, stop_after_attempt
 
-from footcrawl import metrics
-from footcrawl.io import services
+from footcrawl import metrics as metrics_
+from footcrawl import parsers
+from footcrawl.io import datasets, services
 
 Locals = dict[str, T.Any]
 
@@ -23,7 +27,7 @@ class Crawler(abc.ABC, pdt.BaseModel, strict=True, frozen=False, extra="forbid")
     url: str
 
     logger_service: services.LoggerService = services.LoggerService()
-    crawler_metrics: metrics.CrawlerMetrics = metrics.CrawlerMetrics()
+    metrics: metrics_.CrawlerMetrics = metrics_.CrawlerMetrics()
 
     @abc.abstractmethod
     async def crawl(self) -> Locals:
@@ -33,3 +37,70 @@ class Crawler(abc.ABC, pdt.BaseModel, strict=True, frozen=False, extra="forbid")
             Locals: Local crawler variables.
         """
         pass
+
+
+class Engine(pdt.BaseModel, strict=True, frozen=False, extra="forbid"):
+    logger_service: services.LoggerService = services.LoggerService()
+
+    parser: parsers.ParserKind = pdt.Field(...)
+    metrics: metrics_.CrawlerMetrics = pdt.Field(...)
+    output: datasets.WriterKind = pdt.Field(...)
+
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+
+        self._orig_path = self.output.path
+
+    async def write_out(
+        self, session: aiohttp.ClientSession, url: str, season: int
+    ) -> None:
+        logger = self.logger_service.logger()
+
+        async for row in self._parse(session=session, url=url):
+            # format output path
+            formatted_path = self._orig_path.format(season=season)
+            self.output.path = formatted_path
+
+            logger.info("Writing to path: {}", self.output.path)
+            await self.output.write(data=row)
+
+    async def _parse(
+        self, session: aiohttp.ClientSession, url: str
+    ) -> T.AsyncGenerator[parsers.Item, None]:
+        logger = self.logger_service.logger()
+
+        resp = await self._fetch_content(session=session, url=url)
+
+        async for item in self.parser.parse(response=resp):
+            # Record items parsed
+            self.metrics.record_parser(metrics={"items_parsed": 1})
+
+            logger.debug("Parsed item: {}", item)
+            yield item
+
+    @retry(stop=stop_after_attempt(3))
+    async def _fetch_content(
+        self, session: aiohttp.ClientSession, url: str
+    ) -> aiohttp.ClientResponse:
+        resp = await session.get(url)
+        resp.raise_for_status()
+
+        # collecting metrics
+        self.metrics.record_request(resp=resp)
+
+        return resp
+
+    def _check_filepaths(self, seasons: int) -> None:
+        logger = self.logger_service.logger()
+
+        for season in seasons:
+            _output_path = self._orig_path.format(season=season)
+
+            # remove the file before writing to it - don't want duplicates
+            if Path(_output_path).absolute().exists():
+                logger.info("Removing file: ", _output_path)
+                Path(_output_path).absolute().unlink()
+
+    @property
+    def original_path(self) -> str:
+        return self._orig_path
