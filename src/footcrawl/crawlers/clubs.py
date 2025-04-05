e17f3aa812c46e86f617a1a834b8f7ee6bff2c1e
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-05 18:59:31 +0100

    feat(crawlers/clubs.py): added task and file handler logic to crawl method

--- src/footcrawl/crawlers/clubs.py
+++ src/footcrawl/crawlers/clubs.py
@@ -1,12 +1,9 @@
-import asyncio
 import typing as T
 
 import pydantic as pdt
 
 from footcrawl import client, parsers
-from footcrawl import engine as engine_
 from footcrawl.crawlers import base
-from footcrawl.io import datasets
 
 
 class AsyncClubsCrawler(base.Crawler):
@@ -16,19 +13,15 @@ class AsyncClubsCrawler(base.Crawler):
         seasons (list[int]): List of seasons to crawl.
         leagues (list[dict[str, str]]): List of leagues to crawl.
         parser (parsers.ClubsParser): Parser to parse the data.
-        output (datasets.WriterKind): Output writer.
         http_client (client.AsyncClient): HTTP client.
     """
 
-    KIND: T.Literal["AsyncClubsCrawler"] = "AsyncClubsCrawler"
+    KIND: T.Literal["Clubs"] = "Clubs"
 
     # crawler parameters
     seasons: list[int]
     leagues: list[dict[str, str]]
     parser: parsers.ClubsParser = pdt.Field(default_factory=parsers.ClubsParser)
-
-    # io parameter
-    output: datasets.WriterKind = pdt.Field(..., discriminator="KIND")
 
     # client
     http_client: client.AsyncClient
@@ -39,40 +32,32 @@ class AsyncClubsCrawler(base.Crawler):
 
         logger.info("Initialising crawler engine")
 
-        engine = engine_.Engine(
-            parser=self.parser, metrics=self.metrics, output=self.output
-        )
-
-        logger.debug("Engine initialised: {}", engine)
-
-        if engine.output.overwrite:
-            logger.info("Overwrite is: {}", self.output.overwrite)
-            engine._check_filepaths(seasons=self.seasons)
+        self.file_handler.set_original_path(path=self.output.base_path)
+        self.file_handler.check_filepaths(seasons=self.seasons)
 
         async with self.http_client as client:
             session = client.get_session
-            tasks = []
 
             for season in self.seasons:
                 for league in self.leagues:
-                    formatted_url = self.__format_url(league=league, season=season)
-
-                    logger.debug(f"QUEUED: {formatted_url}")
-                    task = asyncio.create_task(
-                        engine.write_out(
-                            session=session, url=formatted_url, season=season
+                    self.task_handler.create_task(
+                        self._write_out(
+                            session=session,
+                            season=season,
+                            url=self._format_url(league=league, season=season),
+                            file_handler=self.file_handler,
                         )
                     )
-                    tasks.append(task)
 
-            await asyncio.gather(*tasks)
+            await self.task_handler.gather_tasks()
 
-        metrics_output = engine.metrics.summary()
+        metrics_output = self.metrics.summary()
         logger.info("Crawler metrics: {}", metrics_output)
 
         return locals()  # returned for testing
 
-    def __format_url(self, league: dict[str, str], season: int) -> str:
+    @T.override
+    def _format_url(self, league: dict[str, str], season: int) -> str:
         return self.url.format(
             league=league.get("name"),
             league_id=league.get("id"),
