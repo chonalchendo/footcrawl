bacd8ba4112b258d8023ac538a535920fedba349
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-09 20:29:42 +0100

    test(test_datasets.py): added base path and new output path to json writer test

--- tests/io/test_datasets.py
+++ tests/io/test_datasets.py
@@ -10,12 +10,12 @@ from footcrawl.io import datasets
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_async_json_writer(tmp_outputs_path: str) -> None:
+async def test_async_json_writer(tmp_base_path: str, tmp_outputs_path: str) -> None:
     # given
     data = {"hello": "world", "foo": "bar"}
 
-    writer = datasets.AsyncJsonWriter(path=tmp_outputs_path)
+    writer = datasets.AsyncNdJsonWriter(base_path=tmp_base_path)
     # when
-    await writer.write(data=data)
+    await writer.write(data=data, output_path=tmp_outputs_path)
     # then
     assert os.path.exists(tmp_outputs_path), "Data should be written!"
