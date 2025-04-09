1874fe9a588b07f469cbe91d2ac0fe57b975784f
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-09 20:27:33 +0100

    test(test_base_crawler.py): added parser and output to testing of base crawler

--- tests/crawlers/test_base_crawler.py
+++ tests/crawlers/test_base_crawler.py
@@ -2,16 +2,19 @@
 
 import pytest
 
-from footcrawl import metrics
+from footcrawl import metrics, parsers
 from footcrawl.crawlers import base
-from footcrawl.io import services
+from footcrawl.io import services, datasets
 
 # %% CRAWLERS
 
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_crawler(
-    logger_service: services.LoggerService, crawler_metrics: metrics.CrawlerMetrics
+    logger_service: services.LoggerService,
+    crawler_metrics: metrics.CrawlerMetrics,
+    clubs_parser: parsers.ClubsParser,
+    tmp_outputs_writer: datasets.AsyncNdJsonWriter,
 ) -> None:
     # given
     url = "https://transfermarkt.co.uk/{league}/startseite/wettbewerb/{league_id}/plus/?saison_id={season}"
@@ -27,6 +30,8 @@ async def test_crawler(
         url=url,
         logger_service=logger_service,
         metrics=crawler_metrics,
+        parser=clubs_parser,
+        output=tmp_outputs_writer,
     )
 
     # when
@@ -36,6 +41,8 @@ async def test_crawler(
     assert hasattr(crawler, "logger_service"), "Crawler should have an Logger service!"
     assert hasattr(crawler, "metrics"), "Crawler should have a metrics class!"
     assert hasattr(crawler, "url"), "Crawler should have a URL!"
+    assert hasattr(crawler, "parser"), "Crawler should have a parser!"
+    assert hasattr(crawler, "output"), "Crawler should have an output!"
 
     # - outputs
     assert set(results) == {"self", "a", "b"}, "Run should return local variables!"
