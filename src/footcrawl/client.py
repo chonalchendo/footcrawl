e4acde5e2e03d528ee55f768e5a41263fa2eecb1
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-18 21:22:09 +0100

    feat(client.py): added timeout functionality

--- src/footcrawl/client.py
+++ src/footcrawl/client.py
@@ -1,9 +1,10 @@
 import aiohttp
 import pydantic as pdt
-from tenacity import retry, stop_after_attempt
 
 from footcrawl import metrics as metrics_
 from footcrawl.io import services
+
+# from tenacity import retry, stop_after_attempt
 
 
 class AsyncClient(pdt.BaseModel, frozen=False, strict=True, extra="forbid"):
@@ -18,6 +19,7 @@ class AsyncClient(pdt.BaseModel, frozen=False, strict=True, extra="forbid"):
     model_config = pdt.ConfigDict(arbitrary_types_allowed=True)
 
     headers: dict[str, str]
+    timeout: int
 
     logger_service: services.LoggerService = services.LoggerService()
     session: aiohttp.ClientSession | None = pdt.Field(default=None)
@@ -32,6 +34,7 @@ class AsyncClient(pdt.BaseModel, frozen=False, strict=True, extra="forbid"):
         if self.session is None:
             logger.info("Initialising client...")
             self.session = aiohttp.ClientSession(
+                timeout=self._set_timeout(self.timeout),
                 headers=self.headers,
             )
         return self
@@ -40,6 +43,10 @@ class AsyncClient(pdt.BaseModel, frozen=False, strict=True, extra="forbid"):
         """Close the client."""
         if self.session and not self.session.closed:
             await self.session.close()
+
+    @staticmethod
+    def _set_timeout(timeout: int) -> aiohttp.ClientTimeout:
+        return aiohttp.ClientTimeout(total=200)
 
     @property
     def get_session(self) -> aiohttp.ClientSession:
@@ -64,21 +71,26 @@ class Response:
         self.url = url
         self.session = session
         self.metrics = metrics
+        self.logger_service: services.LoggerService = services.LoggerService()
 
     async def __call__(self, *args, **kwds):
         return await self._fetch_response()
 
-    @retry(stop=stop_after_attempt(3))
+    # @retry(stop=stop_after_attempt(3))
     async def _fetch_response(self) -> aiohttp.ClientResponse:
+        logger = self.logger_service.logger()
         try:
-            resp = await self.session.get(self.url)
+            logger.info("Requesting URL: {}", self.url)
+            resp = await self.session.get(self.url, timeout=180)
             resp.raise_for_status()
             return resp
         except aiohttp.ClientError as e:
-            raise aiohttp.ClientError(f"Failed to fetch {self.url}: {e}")
+            error = aiohttp.ClientError(f"Failed to fetch {self.url}: {e}")
+            logger.error(error)
         except aiohttp.ClientResponseError as e:
-            raise aiohttp.ClientResponseError(
+            error = aiohttp.ClientResponseError(
                 f"HTTP error {e.status} for {self.url}: {e.message}"
             )
+            logger.error(error)
         finally:
             self.metrics.record_request(resp=resp)
