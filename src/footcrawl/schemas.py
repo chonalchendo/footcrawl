229b5d8286b5d9ee1f4e57336f32b6e284899b42
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-18 08:43:51 +0100

    style(schemas.py): reformatted file

--- src/footcrawl/schemas.py
+++ src/footcrawl/schemas.py
@@ -6,42 +6,42 @@ import pydantic as pdt
 class ClubsSchema(pdt.BaseModel):
     """A Pydantic class to validate parsed clubs data."""
 
-    club_id: str 
-    club_tm_name: str 
-    club: str 
-    club_link: str 
-    season: str 
-    comp_id: str 
-    comp_name: str 
-    squad_size: str 
-    average_age: str 
-    foreign_players: str 
-    average_player_value: str 
-    total_player_value: str 
+    club_id: str
+    club_tm_name: str
+    club: str
+    club_link: str
+    season: str
+    comp_id: str
+    comp_name: str
+    squad_size: str
+    average_age: str
+    foreign_players: str
+    average_player_value: str
+    total_player_value: str
 
 
 class SquadsSchema(pdt.BaseModel):
-    player_id: str 
-    player_tm_name: str 
-    player_name: str 
-    club_name: str 
-    season: str 
-    url: str 
-    position: str 
-    link: str 
-    injury_note: str | None 
-    market_value: str 
-    previous_value: str | None 
-    number: str 
-    dob: str 
-    age: str 
-    nationalities: list[str] 
-    height: str | None 
-    foot: str | None 
-    signed_date: str | None 
-    transfer_fee: str | None 
-    signed_from: str | None 
-    contract_expiry: str | None 
+    player_id: str
+    player_tm_name: str
+    player_name: str
+    club_name: str
+    season: str
+    url: str
+    position: str
+    link: str
+    injury_note: str | None
+    market_value: str
+    previous_value: str | None
+    number: str
+    dob: str
+    age: str
+    nationalities: list[str]
+    height: str | None
+    foot: str | None
+    signed_date: str | None
+    transfer_fee: str | None
+    signed_from: str | None
+    contract_expiry: str | None
 
 
 class FixturesSchema(pdt.BaseModel):
@@ -67,24 +67,24 @@ class FixturesSchema(pdt.BaseModel):
 
 
 class MatchLineupsSchema(pdt.BaseModel):
-    match_id: str 
-    matchday: str 
-    comp_id: str 
-    comp_name: str 
-    club_id: str 
-    club_name: str 
-    club_tm_name: str 
-    player_id: str 
-    player_tm_name: str 
-    player_name: str 
-    number: str 
-    position: str 
-    current_value: str 
-    country: str 
-    misc: str 
-    profile_link: str 
-    season_stats_link: str 
-    starter: bool 
+    match_id: str
+    matchday: str
+    comp_id: str
+    comp_name: str
+    club_id: str
+    club_name: str
+    club_tm_name: str
+    player_id: str
+    player_tm_name: str
+    player_name: str
+    number: str
+    position: str
+    current_value: str
+    country: str
+    misc: str
+    profile_link: str
+    season_stats_link: str
+    starter: bool
 
 
 class MatchActionsSchema(pdt.BaseModel):
@@ -126,23 +126,23 @@ class _SubAction(pdt.BaseModel):
 
 
 class _CardAction(pdt.BaseModel):
-    club_id: str 
-    club_tm_name: str 
-    club_name: str 
-    player_id: str 
-    player_name: str 
-    player_tm_name: str 
-    card_type: str 
-    reason: str 
-    player_card_type_season_total: str 
+    club_id: str
+    club_tm_name: str
+    club_name: str
+    player_id: str
+    player_name: str
+    player_tm_name: str
+    card_type: str
+    reason: str
+    player_card_type_season_total: str
 
 
 class MatchStatsSchema(pdt.BaseModel):
-    match_id: str 
-    matchday: str 
-    comp_id: str 
-    comp_tm_name: str 
-    season: str 
+    match_id: str
+    matchday: str
+    comp_id: str
+    comp_tm_name: str
+    season: str
     home_team_stats: _StatsSchema
     away_team_stats: _StatsSchema
 
@@ -150,13 +150,13 @@ class MatchStatsSchema(pdt.BaseModel):
 class _StatsSchema(pdt.BaseModel):
     club_id: str
     club_tm_name: str
-    total_shots: str 
-    shots_off_target: str 
-    shots_saved: str 
-    corners: str 
-    free_kicks: str 
-    fouls: str 
-    offsides: str 
+    total_shots: str
+    shots_off_target: str
+    shots_saved: str
+    corners: str
+    free_kicks: str
+    fouls: str
+    offsides: str
 
 
 SchemaKind = (
