2e4cdd310bd2490bb053b22f58bbc07078f7c961
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-04-18 21:20:45 +0100

    feat(parsers): added match lineups parser to init file

--- src/footcrawl/parsers/__init__.py
+++ src/footcrawl/parsers/__init__.py
@@ -1,8 +1,15 @@
 from .base import Item
 from .clubs import ClubsParser
 from .fixtures import FixturesParser
+from .match_lineups import MatchLineupsParser
 from .squads import SquadsParser
 
-ParserKind = ClubsParser | SquadsParser | FixturesParser
+ParserKind = ClubsParser | SquadsParser | FixturesParser | MatchLineupsParser
 
-__all__ = ["ClubsParser", "SquadsParser", "FixturesParser", "Item"]
+__all__ = [
+    "ClubsParser",
+    "SquadsParser",
+    "FixturesParser",
+    "MatchLineupsParser",
+    "Item",
+]
