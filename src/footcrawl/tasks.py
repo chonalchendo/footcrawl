561d4885ca3ceeaec0cacc53c5b216db677f480a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-05-16 19:04:13 +0100

    feat(tasks.py): added in concurrency limiting and sleeping between batches functionality to avoid making too many concurrent requests

--- src/footcrawl/tasks.py
+++ src/footcrawl/tasks.py
@@ -9,12 +9,30 @@ class TaskHandler(pdt.BaseModel, strict=True, frozen=False, extra="forbid"):
 
     tasks: list[asyncio.Task] = pdt.Field(default_factory=list)
 
+    max_concurrency: int = pdt.Field(default=None)
+    time_between_batches: int = pdt.Field(default=None)
+
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+
+        self._semaphore: asyncio.Semaphore = asyncio.Semaphore(self.max_concurrency)
+
     async def gather_tasks(self) -> None:
         await asyncio.gather(*self.tasks)
 
     def create_task(self, coroutine: T.Coroutine) -> None:
-        task = asyncio.create_task(coroutine)
+        task = asyncio.create_task(
+            _concurrency_limiter(coroutine, self._semaphore, self.time_between_batches)
+        )
         self._add_task(task)
 
     def _add_task(self, task: asyncio.Task) -> None:
         self.tasks.append(task)
+
+
+async def _concurrency_limiter(
+    couroutine: T.Coroutine, semaphore: asyncio.Semaphore, sleep: int
+) -> None:
+    async with semaphore:
+        await couroutine
+        await asyncio.sleep(sleep)
